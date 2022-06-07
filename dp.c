e594802c43b5654aa74e72827fcd9368601f3b7e
hiro2001 <hiroyukikorea@gmail.com>
2022-06-07 11:23:45 +0900

    Ad

--- dp.c
+++ dp.c
@@ -5,7 +5,6 @@
 #define MEMORY 256
 #define DIMENTION 15
 #define NUMOFFILE 100
-#define FNAME_OUTPUT "./output001.txt"
 
 typedef struct {
     char name[20];
@@ -14,33 +13,55 @@ typedef struct {
     double mcepdata[MEMORY][DIMENTION];
 } mcepdata_t;
 
+void print(int i1, int i2, int m, int n, double d){
+	FILE *fp;
+	fp = fopen("log.log", "a+");
+	if (fp == NULL) {
+		printf("error\n");
+		exit(EXIT_FAILURE);
+	}
+
+  fprintf(fp, "city_temp : city%3d/city%3d_%3d.txt\n", m, m, i1 + 1);
+  fprintf(fp, "city_miti : city%3d/city%3d_%3d.txt\n", n, n, i2 + 1);
+	printf("Between words : %f\n", d);
+  fprintf(fp, "Between words : %f\n\n", d);
+	printf("...Roading now...\n");
+	fclose(fp);
+}
+
+void scan(int* i, int*m){
+	printf("ファイル指定\n");
+	printf("city0");
+	scanf("%03d", i);
+	printf("city0");
+	scanf("%03d", m);
+}
+
 int main(void){
 	int  h0, h, i, j, k;
-	FILE *fp_temp, *fp_miti, *fp_output;
-	mcepdata_t city_temp, city_miti; //テンプレートのデータと未知入力データ
+	FILE *fp_temp, *fp_miti;
+	mcepdata_t city_temp, city_miti;
 	char ch0[200]; 
-	double d[MEMORY][MEMORY];      //局所距離
-	double g[MEMORY][MEMORY];      //累積距離
-	double distance[NUMOFFILE]; //単語間距離
-	double distance_min; 
+	double d[MEMORY][MEMORY];
+	double g[MEMORY][MEMORY];
+	double distance[NUMOFFILE];
+	double distance_min, a, b, c; 
 	int num_matchfname = 0;
 	int count = 0;
 	int TEMP_NUM, MITI_NUM;
-
-	printf("ファイルを選択してください\n");
-	printf("city0");
-	scanf("%03d", &TEMP_NUM);
-	printf("city0");
-	scanf("%03d", &MITI_NUM);
-	printf("city%03dとcity%03dの認識実験を開始します。\n", TEMP_NUM, MITI_NUM);
-
-
-
+	
+	scan(&TEMP_NUM, &MITI_NUM);
+	fp_temp = fopen("log.log", "w");
+	if (fp_temp == NULL) {
+		printf("error\n");
+		exit(EXIT_FAILURE);
+	}
+	fclose(fp_temp);
 	for (h0 = 0; h0 < NUMOFFILE; h0++) {
 		sprintf(ch0, "./city%03d/city%03d_%03d.txt",TEMP_NUM, TEMP_NUM, h0 + 1);
-	
-		if ((fp_temp = fopen(ch0, "r")) == NULL) {
-			printf("temp file open error!!\n");
+		fp_temp = fopen(ch0, "r");
+		if (fp_temp == NULL) {
+			printf("error\n");
 			exit(EXIT_FAILURE);
 		}
 
@@ -77,8 +98,7 @@ int main(void){
 			for (i = 0; i < city_temp.flame; i++) {
 				for (j = 0; j < city_miti.flame; j++) {
         	d[i][j] = 0;
-					for (int k = 0; k < DIMENTION; k++) {
-						//printf("%f\n", kyokusyokyori);
+					for (k = 0; k < DIMENTION; k++) {
 						d[i][j] += (city_temp.mcepdata[i][k] - city_miti.mcepdata[j][k]) * (city_temp.mcepdata[i][k] - city_miti.mcepdata[j][k]); 
 					}
           sqrtl(d[i][j]);
@@ -95,9 +115,9 @@ int main(void){
 			
 			for (i = 1; i < city_temp.flame; i++) {
 				for (j = 1; j < city_miti.flame; j++) {
-					double a = g[i][j - 1] + d[i][j];
-					double b = g[i - 1][j - 1] + 2 * d[i][j];
-					double c = g[i - 1][j] + d[i][j];
+					a = g[i][j - 1] + d[i][j];
+					b = g[i - 1][j - 1] + 2 * d[i][j];
+					c = g[i - 1][j] + d[i][j];
 					g[i][j] = a;
 					if (b < g[i][j]) {
 						g[i][j] = b;
@@ -110,45 +130,22 @@ int main(void){
 			distance[h] = g[city_temp.flame - 1][city_miti.flame - 1] / (city_temp.flame + city_miti.flame);
 			fclose(fp_miti);
 		}
-
       distance_min = distance[0];
       num_matchfname = 0;
 		for (h = 1; h < NUMOFFILE; h++) {
 			if (distance_min > distance[h] ) {
-				tangokankyori_min = tangokankyori[h];
+				distance_min = distance[h];
 				num_matchfname = h;
 			}
 		}
-
-		
 		fclose(fp_temp);
-
-		
 		if (num_matchfname == h0 ) {
 			count++;
 		}
-
 		if (num_matchfname != h0 ) {
-   		printf("----------Result NOT Matchng----------\n");
-     	printf("city_temp     : city%03d/city%03d_%03d.txt\n", TEMP_NUM, TEMP_NUM, h0 + 1);
-    	printf("city_miti     : city%03d/city%03d_%03d.txt\n", MITI_NUM, MITI_NUM, num_matchfname + 1);
-      printf("Between words : %f\n", distance_min);
-    }
-
+    	print(h0,num_matchfname, TEMP_NUM, MITI_NUM, distance_min);
+		}
 	}
-
-	sprintf(ch0, FNAME_OUTPUT);
-	
-	if ((fp_output = fopen(ch0, "a")) == NULL) {
-		printf("output file open error!!\n");
-		exit(EXIT_FAILURE);
-	}
-
-	fprintf(fp_output, "正答率%d%%です。\n", count);
-  printf("\nファイルを作成しました。\n");
-	printf("正答率 %d%% です。\n", count); 
-
-	fclose(fp_output);
-
+	printf("正答率 %d%%\n", count); 
 	return 0;
 }
